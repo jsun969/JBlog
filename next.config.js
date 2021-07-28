2852ee0cccd291837d7313e54e6888ce04e5df3b
jsun969 <i@jsun969.cn>
2021-07-28 18:45:32 +0800

    :tada: 初始化项目

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const withTM = require('next-transpile-modules');
+
+module.exports = withTM(['@material-ui/core', '@material-ui/icons'])({
+  transpileModules: ['@material-ui/core', '@material-ui/icons'],
+});
+
 module.exports = {
   reactStrictMode: true,
-}
+};
