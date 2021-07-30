02075945b01627db94c8b9d3c2faf304570bcf09
jsun969 <i@jsun969.cn>
2021-07-30 14:10:51 +0800

    :bug: 修复刷新导致样式失效的问题
    - 使用MUI的cssbaseline代替normalize.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import type { AppProps } from 'next/app';
-import 'normalize.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../src/theme';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
+
+export default theme;
