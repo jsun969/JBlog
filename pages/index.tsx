c9abbd335f3513303c888684ca69f0ecdfa52b04
jsun969 <i@jsun969.cn>
2021-07-30 17:03:08 +0800

    :pencil2: 修改主页标签栏标题

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>荆棘小栈 - 荆棘的博客</title>
+        <title>荆棘小栈 - 主页</title>
         <meta name="description" content="欢迎访问荆棘的个人博客,在这里,会分享我生活和编程的点滴时光" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
