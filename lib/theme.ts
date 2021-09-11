6ca4acc8ae1998f22471c5d281dfb4631d03fc85
jsun969 <i@jsun969.cn>
2021-09-11 11:21:39 +0800

    :lipstick: 优化主题导入

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,13 +1,10 @@
+import { indigo, pink } from '@material-ui/core/colors';
 import { createTheme } from '@material-ui/core/styles';
 
 const theme = createTheme({
   palette: {
-    primary: {
-      main: '#3f51b5',
-    },
-    secondary: {
-      main: '#f50057',
-    },
+    primary: indigo,
+    secondary: pink,
   },
 });
 
