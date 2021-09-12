8b816262376cc8e9cca9dbcdb4f9ddc0669805a1
jsun969 <i@jsun969.cn>
2021-09-12 22:37:38 +0800

    :adhesive_bandage: 修改修改关于api返回值

--- pages/api/graphql/resolvers/modifyAbout.ts
+++ pages/api/graphql/resolvers/modifyAbout.ts
@@ -3,5 +3,5 @@ import prisma from '../../../../lib/prisma';
 export async function modifyAbout(_: unknown, { content }: { content: string }, { isAdmin }: { isAdmin: boolean }) {
   if (!isAdmin) return null;
   await prisma.about.upsert({ where: { id: 0 }, update: { content }, create: { content } });
-  return 0;
+  return true;
 }
