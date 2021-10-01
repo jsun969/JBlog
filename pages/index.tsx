02c8c729f34866b0e41ab855d7e9a2d38b320d12
jsun969 <i@jsun969.cn>
2021-10-01 21:16:00 +0800

    :pencil2: 主页描述添加逗号

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ const HomePage: NextPage<HomePageProps> = ({ articles, bulletin }) => {
     <>
       <Head>
         <title>荆棘小栈 - 主页</title>
-        <meta name="description" content="欢迎访问荆棘的个人博客,在这里,会分享我生活和编程的点滴时光" />
+        <meta name="description" content="欢迎访问荆棘的个人博客, 在这里, 会分享我生活和编程的点滴时光" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout title="荆棘小栈" select="index">
