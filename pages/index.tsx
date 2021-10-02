86a45abd64197ce441d3ac2824049ebe37879b39
jsun969 <i@jsun969.cn>
2021-10-02 09:47:38 +0800

    :pencil2: title移除主页

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ const HomePage: NextPage<HomePageProps> = ({ articles, bulletin }) => {
   return (
     <>
       <Head>
-        <title>荆棘小栈 - 主页</title>
+        <title>荆棘小栈</title>
         <meta name="description" content="欢迎访问荆棘的个人博客, 在这里, 会分享我生活和编程的点滴时光" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
