08fb3f6e8204160a042267ba6921bf3c6188d46e
jsun969 <i@jsun969.cn>
2021-11-09 19:45:11 +0800

    :sparkles: 给代码块添加语言标识

--- components/markdown/Code.tsx
+++ components/markdown/Code.tsx
@@ -5,7 +5,8 @@ interface CodeProps {
 
 const Code: React.FC<CodeProps> = ({ language, children }) => {
   return (
-    <pre style={{ borderRadius: 4 }}>
+    <pre style={{ borderRadius: 4, position: 'relative' }}>
+      <div style={{ position: 'absolute', top: 4, right: 8, color: 'lightslategray' }}>{language}</div>
       <code
         style={{ backgroundColor: '#f6f8fa' }}
         className={`hljs language-${language}`}
